4fdec82a9bd352f55949681b332d0030494d4575
Jonathan Richmond <richmo11@purdue.edu>
2023-09-14 14:48:43 -0400

    Updated TBPEquationsOfMotion functions

--- src/TBP/EquationsOfMotion.jl
+++ src/TBP/EquationsOfMotion.jl
@@ -24,9 +24,9 @@ Return time derivative of state vector
 function computeDerivatives!(qdot::Vector{Float64}, q::Vector{Float64}, EOMs::TBPEquationsOfMotion, t::Float64)
     r::Float64 = sqrt(q[1]^2+q[2]^2+q[3]^2)
     qdot[1:3] = q[4:6]
-    qdot[4:6] = -EOMs.mu.*q[1:3]./r^3
+    qdot[4:6] = -1*q[1:3]./r^3
     if (EOMs.equationType == MBD.STM) || (EOMs.equationType == MBD.FULL)
-        A_21 = (3*EOMs.mu/r^5).*q[1:3]'*q[1:3]-(EOMs.mu/r^3).*LinearAlgebra.I
+        A_21 = (3*1/r^5).*q[1:3]'*q[1:3]-(1/r^3).*LinearAlgebra.I
         qdot[7:42] = vec([zeros(Float64, (3,3)) LinearAlgebra.I; A_21 zeros(Float64, (3,3))])
     end
 end
