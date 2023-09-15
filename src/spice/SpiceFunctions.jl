5e9f7aaf9805e438e3c0b385de978e21c410fe03
Jonathan Richmond <richmo11@purdue.edu>
2023-09-15 00:43:23 -0400

    Updated SpiceFunctions.jl

--- src/spice/SpiceFunctions.jl
+++ src/spice/SpiceFunctions.jl
@@ -21,14 +21,14 @@ Return ephemerides
 - `observerBody::String`: Body for ephemerides reference
 - `frame::String`: Reference frame
 """
-function getEphemerides(initialEpoch::String, times::Vector{Float64}, targetBody::String, observerBody::String, frame::String)
-    SPICE.furnsh("MBD.jl/src/spice/kernels/naif0012.tls", "MBD.jl/src/spice/kernels/de440.bsp", "MBD.jl/src/spice/kernels/mar097.bsp")
+function getEphemerides(initialEpoch::String, times::Vector{Float64}, targetBody::String, observerBody::String, frame::String, directory::String)
+    SPICE.furnsh(directory*"src/spice/kernels/naif0012.tls", directory*"src/spice/kernels/de440.bsp", directory*"src/spice/kernels/mar097.bsp")
     epoch::Float64 = SPICE.str2et(initialEpoch)
     epochs::Vector{Float64} = epoch.+times
     ephemerisStates::Vector{Vector{Float64}} = Vector{Vector{Float64}}(undef, length(epochs))
     ephemerisTimes::Vector{Float64} = Vector{Float64}(undef, length(epochs))
     for e::Int64 in 1:length(epochs)
-        (state::Vector{Float64}, time::Float64) = SPICE.spkezr(targetBody, epochs[e], "ECLIPJ2000", "NONE", "SUN")
+        (state::Vector{Float64}, time::Float64) = SPICE.spkezr(targetBody, epochs[e], frame, "NONE", observerBody)
         ephemerisStates[e] = state
         ephemerisTimes[e] = time
     end
