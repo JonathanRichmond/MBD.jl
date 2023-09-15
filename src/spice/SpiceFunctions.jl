a1a481fae6c797c448cf3bf663ad958fac6ea27d
Jonathan Richmond <richmo11@purdue.edu>
2023-09-14 22:06:56 -0400

    Updated SpiceFunctions.jl

--- src/spice/SpiceFunctions.jl
+++ src/spice/SpiceFunctions.jl
@@ -25,10 +25,10 @@ function getEphemerides(initialEpoch::String, times::Vector{Float64}, targetBody
     SPICE.furnsh("MBD.jl/src/spice/kernels/naif0012.tls", "MBD.jl/src/spice/kernels/de440.bsp", "MBD.jl/src/spice/kernels/mar097.bsp")
     epoch::Float64 = SPICE.str2et(initialEpoch)
     epochs::Vector{Float64} = epoch.+times
-    ephemerisStates::Vector{Float64} = Vector{Float64}(undef, length(times))
-    ephemerisTimes::Vector{Float64} = Vector{Float64}(undef, length(times))
-    for e::Int64 in length(times)
-        (state::Vector{Float64}, time::Float64) = SPICE.spkezr(targetBody, epochs[e], frame, "NONE", observerBody)
+    ephemerisStates::Vector{Vector{Float64}} = Vector{Vector{Float64}}(undef, length(epochs))
+    ephemerisTimes::Vector{Float64} = Vector{Float64}(undef, length(epochs))
+    for e::Int64 in 1:length(epochs)
+        (state::Vector{Float64}, time::Float64) = SPICE.spkezr(targetBody, epochs[e], "ECLIPJ2000", "NONE", "SUN")
         ephemerisStates[e] = state
         ephemerisTimes[e] = time
     end
