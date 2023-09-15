02ac7c467312db62a417d84e5c932b0db42f0fd9
Jonathan Richmond <richmo11@purdue.edu>
2023-09-14 21:51:17 -0400

    Updated SpiceFunctions.jl

--- src/spice/SpiceFunctions.jl
+++ src/spice/SpiceFunctions.jl
@@ -23,9 +23,15 @@ Return ephemerides
 """
 function getEphemerides(initialEpoch::String, times::Vector{Float64}, targetBody::String, observerBody::String, frame::String)
     SPICE.furnsh("MBD.jl/src/spice/kernels/naif0012.tls", "MBD.jl/src/spice/kernels/de440.bsp", "MBD.jl/src/spice/kernels/mar097.bsp")
-    epoch = SPICE.str2et(initialEpoch)
-    epochs = epoch.+times
-    (states, times) = SPICE.spkezr(targetBody, epochs, frame, "NONE", observerBody)
+    epoch::Float64 = SPICE.str2et(initialEpoch)
+    epochs::Vector{Float64} = epoch.+times
+    ephemerisStates::Vector{Float64} = Vector{Float64}(undef, length(times))
+    ephemerisTimes::Vector{Float64} = Vector{Float64}(undef, length(times))
+    for e::Int64 in length(times)
+        (state::Vector{Float64}, time::Float64) = SPICE.spkezr(targetBody, epochs[e], frame, "NONE", observerBody)
+        ephemerisStates[e] = state
+        ephemerisTimes[e] = time
+    end
     SPICE.kclear()
     
     return (states, times)
