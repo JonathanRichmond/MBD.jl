2af350679999233f2f21bb9cd38e3237e981ba67
Jonathan Richmond <richmo11@purdue.edu>
2023-09-21 00:14:40 -0400

    Updated EventFunctions.jl

--- src/propagation/EventFunctions.jl
+++ src/propagation/EventFunctions.jl
@@ -7,10 +7,10 @@ C: 9/20/23
 
 import DifferentialEquations
 
-export arclengthCondition, terminateAffect!
+export arclengthCondition!, terminateAffect!
 
 """
-    arclengthCondition(state, time, integrator)
+    arclengthCondition!(state, time, integrator)
 
 Return event condition
 
@@ -19,12 +19,12 @@ Return event condition
 - `time::Float64`: Time [ndim]
 - `integrator::DEIntegrator`: Integrator object
 """
-function arclengthCondition(state::Vector{Float64}, time::Float64, integrator::DifferentialEquations.DEIntegrator)
+function arclengthCondition!(state::Vector{Float64}, time::Float64, integrator::DifferentialEquations.DEIntegrator)
     state[43]-integrator.opts.userdata[:conditionValue]
 end
 
 """
-    terminateAffect(integrator)
+    terminateAffect!(integrator)
 
 Return event effect
 
