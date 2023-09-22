c8b764b1556747c2f2c5fbaed463e985249a59f8
Jonathan Richmond <richmo11@purdue.edu>
2023-09-21 20:56:19 -0400

    Updated TBPDynamicsModel functions

--- src/TBP/DynamicsModel.jl
+++ src/TBP/DynamicsModel.jl
@@ -54,7 +54,7 @@ Return time derivative of state vector
 function evaluateEquations(dynamicsModel::TBPDynamicsModel, equationType::MBD.EquationType, t::Float64, q::Vector{Float64})
     qdot::Vector{Float64} = Vector{Float64}(undef, getStateSize(dynamicsModel, equationType))
     EOMs::MBD.TBPEquationsOfMotion = getEquationsOfMotion(dynamicsModel, equationType)
-    computeDerivatives!(qdot, q, EOMs, t)
+    computeDerivatives!(qdot, q, (EOMs,), t)
 
     return qdot
 end
