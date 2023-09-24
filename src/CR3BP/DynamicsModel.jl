658281cbab8b60819889fc7fe20c09e1f84555ad
Jonathan Richmond <richmo11@purdue.edu>
2023-09-23 23:07:01 -0400

    Updated CR3BPDynamicsModel functions

--- src/CR3BP/DynamicsModel.jl
+++ src/CR3BP/DynamicsModel.jl
@@ -370,7 +370,7 @@ function rotating2PrimaryInertial(dynamicsModel::CR3BPDynamicsModel, primary::In
     (1 <= primary <= 2) || throw(ArgumentError("Invalid primary $primary"))
     states_primaryInertial::Vector{Vector{Float64}} = Vector{Vector{Float64}}(undef, length(times))
     for i in 1:length(times)
-        state_primary = states[i]+push!(getPrimaryPosition(dynamicsModel, primary), 0, 0, 0)
+        state_primary = states[i]-push!(getPrimaryPosition(dynamicsModel, primary), 0, 0, 0)
         C::Matrix{Float64} = [cos(times[i]) -sin(times[i]) 0; sin(times[i]) cos(times[i]) 0; 0 0 1]
         Cdot::Matrix{Float64} = [-sin(times[i]) -cos(times[i]) 0; cos(times[i]) -sin(times[i]) 0; 0 0 0]
         N::Matrix{Float64} = [C zeros(Float64, (3,3)); Cdot C]
