68d8f94236b4fc3860023be3d3c382f1e54d7f84
Jonathan Richmond <richmo11@purdue.edu>
2023-09-29 00:52:49 -0400

    Updated CR3BPDynamicsModel functions

--- src/CR3BP/DynamicsModel.jl
+++ src/CR3BP/DynamicsModel.jl
@@ -371,7 +371,7 @@ Return primary-centered Ecliptic J2000 inertial frame states [ndim]
 function rotating2PrimaryEclipJ2000(dynamicsModel::CR3BPDynamicsModel, initialEpoch::String, center::BodyData, bodyPlane::BodyData, states::Vector{Vector{Float64}}, times::Vector{Float64})
     (length(states) == length(times)) || throw(ArgumentError("Number of state vectors, $(length(states)), must match number of times, $(length(times))"))
     (bodyInitialStateDim::Vector{Vector{Float64}}, initialEpochDim::Vector{Float64}) = getEphemerides(initialEpoch, [0.0], bodyPlane.name, center.name, "ECLIPJ2000", "MBD.jl/")
-    bodySPICEElements::Vector{Float64} = SPICE.oscltx(bodyInitialStateDim[1], SPICE.str2et(epoch), center.gravParam)
+    bodySPICEElements::Vector{Float64} = SPICE.oscltx(bodyInitialStateDim[1], SPICE.str2et(initialEpoch), center.gravParam)
     timesDim::Vector{Float64} = times.*dynamicsModel.systemData.charTime
     bodyLength::Float64 = bodyPlane.orbitRadius
     bodyTime::Float64 = sqrt(bodyLength^3/(center.gravParam+bodyPlane.gravParam))
