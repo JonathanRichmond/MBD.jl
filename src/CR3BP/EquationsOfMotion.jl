d522212123aab435bc54beb91f1a3b542d47aedc
Jonathan Richmond <richmo11@purdue.edu>
2024-02-07 02:44:29 -0500

    Added Taylor integration

--- src/CR3BP/EquationsOfMotion.jl
+++ src/CR3BP/EquationsOfMotion.jl
@@ -76,23 +76,25 @@ TaylorIntegration.@taylorize function computeTaylorDerivatives!(qdot::Vector{Flo
     qdot[4] = (((2*q[5])+q[1])-((omm*(q[1]+mu))/r_13_3))-((mu*(q[1]-omm))/r_23_3)
     qdot[5] = ((q[2]-(2*q[4]))-((omm*q[2])/r_13_3))-((mu*q[2])/r_23_3)
     qdot[6] = ((-omm*q[3])/r_13_3)-((mu*q[3])/r_23_3)
-    r_13_5::Float64 = r_13_3*(r_13^2)
-    r_23_5::Float64 = r_23_3*(r_23^2)
-    pseudoPotentialJacobian::Vector{Float64} = Vector{Float64}(undef, 6)
-    pseudoPotentialJacobian[1] = (((1-(omm/r_13_3))-(mu/r_23_3))+(((3*omm)*((q[1]+mu)^2))/r_13_5))+(((3*mu)*((q[1]-omm)^2))/r_23_5)
-    pseudoPotentialJacobian[2] = (((1-(omm/r_13_3))-(mu/r_23_3))+(((3*omm)*(q[2]^2))/r_13_5))+(((3*mu)*(q[2]^2))/r_23_5)
-    pseudoPotentialJacobian[3] = (((-omm/r_13_3)-(mu/r_23_3))+(((3*omm)*(q[3]^2))/r_13_5))+(((3*mu)*(q[3]^2))/r_23_5)
-    pseudoPotentialJacobian[4] = ((((3*omm)*(q[1]+mu))*q[2])/r_13_5)+((((3*mu)*(q[1]-omm))*q[2])/r_23_5)
-    pseudoPotentialJacobian[5] = ((((3*omm)*(q[1]+mu))*q[3])/r_13_5)+((((3*mu)*(q[1]-omm))*q[3])/r_23_5)
-    pseudoPotentialJacobian[6] = ((((3*omm)*q[2])*q[3])/r_13_5)+((((3*mu)*q[2])*q[3])/r_23_5)
-    for r::Int64 in 1:3
-        for c::Int64 in 1:6
-            qdot[(6+(6*(c-1)))+r] = q[(9+(6*(c-1)))+r]
-        end
-    end
-    for c in 1:6
-        qdot[10+(6*(c-1))] = (((pseudoPotentialJacobian[1]*q[7+(6*(c-1))])+(pseudoPotentialJacobian[4]*q[8+(6*(c-1))]))+(pseudoPotentialJacobian[5]*q[9+(6*(c-1))]))+(2*q[11+(6*(c-1))])
-        qdot[11+(6*(c-1))] = (((pseudoPotentialJacobian[4]*q[7+(6*(c-1))])+(pseudoPotentialJacobian[2]*q[8+(6*(c-1))]))+(pseudoPotentialJacobian[6]*q[9+(6*(c-1))]))-(2*q[10+(6*(c-1))])
-        qdot[12+(6*(c-1))] = ((pseudoPotentialJacobian[5]*q[7+(6*(c-1))])+(pseudoPotentialJacobian[6]*q[8+(6*(c-1))]))+(pseudoPotentialJacobian[3]*q[9+(6*(c-1))])
-    end
+    #r_13_5::Float64 = r_13_3*(r_13^2)
+    #r_23_5::Float64 = r_23_3*(r_23^2)
+    #pseudoPotentialJacobian::Vector{Float64} = Vector{Float64}(undef, 6)
+    #pseudoPotentialJacobian[1] = (((1-(omm/r_13_3))-(mu/r_23_3))+(((3*omm)*((q[1]+mu)^2))/r_13_5))+(((3*mu)*((q[1]-omm)^2))/r_23_5)
+    #pseudoPotentialJacobian[2] = (((1-(omm/r_13_3))-(mu/r_23_3))+(((3*omm)*(q[2]^2))/r_13_5))+(((3*mu)*(q[2]^2))/r_23_5)
+    #pseudoPotentialJacobian[3] = (((-omm/r_13_3)-(mu/r_23_3))+(((3*omm)*(q[3]^2))/r_13_5))+(((3*mu)*(q[3]^2))/r_23_5)
+    #pseudoPotentialJacobian[4] = ((((3*omm)*(q[1]+mu))*q[2])/r_13_5)+((((3*mu)*(q[1]-omm))*q[2])/r_23_5)
+    #pseudoPotentialJacobian[5] = ((((3*omm)*(q[1]+mu))*q[3])/r_13_5)+((((3*mu)*(q[1]-omm))*q[3])/r_23_5)
+    #pseudoPotentialJacobian[6] = ((((3*omm)*q[2])*q[3])/r_13_5)+((((3*mu)*q[2])*q[3])/r_23_5)
+    #for r::Int64 in 1:3
+    #    for c::Int64 in 1:6
+    #        qdot[(6+(6*(c-1)))+r] = q[(9+(6*(c-1)))+r]
+    #    end
+    #end
+    #for c in 1:6
+    #    qdot[10+(6*(c-1))] = (((pseudoPotentialJacobian[1]*q[7+(6*(c-1))])+(pseudoPotentialJacobian[4]*q[8+(6*(c-1))]))+(pseudoPotentialJacobian[5]*q[9+(6*(c-1))]))+(2*q[11+(6*(c-1))])
+    #    qdot[11+(6*(c-1))] = (((pseudoPotentialJacobian[4]*q[7+(6*(c-1))])+(pseudoPotentialJacobian[2]*q[8+(6*(c-1))]))+(pseudoPotentialJacobian[6]*q[9+(6*(c-1))]))-(2*q[10+(6*(c-1))])
+    #    qdot[12+(6*(c-1))] = ((pseudoPotentialJacobian[5]*q[7+(6*(c-1))])+(pseudoPotentialJacobian[6]*q[8+(6*(c-1))]))+(pseudoPotentialJacobian[3]*q[9+(6*(c-1))])
+    #end
+    
+    return nothing
 end
