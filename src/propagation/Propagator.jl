03501df097a1ed40455e7883adbfb54ac8cc0da7
Jonathan Richmond <richmo11@purdue.edu>
2025-02-20 13:27:47 -0500

    Added BCR4BP P4-B1 propagation capabilities

--- src/propagation/Propagator.jl
+++ src/propagation/Propagator.jl
@@ -72,6 +72,36 @@ function propagate(propagator::Propagator, q0::Vector{Float64}, tSpan::Vector{Fl
 end
 
 """
+    propagate(propagator, q0, tSpan, dynamicsModel)
+
+Return propagated BCR4BP P4-B1 arc
+
+# Arguments
+- `propagator::Propagator`: Propagator object
+- `q0::Vector{Float64}`: Initial state vector [ndim]
+- `tSpan::Vector{Float64}`: Time span [ndim]
+- `dynamicsModel::BCR4BP41DynamicsModel`: BCR4BP P4-B1 dynamics model object
+"""
+function propagate(propagator::Propagator, q0::Vector{Float64}, tSpan::Vector{Float64}, dynamicsModel::MBD.BCR4BP41DynamicsModel)
+    arcOut = MBD.BCR4BP41Arc(dynamicsModel)
+    EOMs::MBD.BCR4BP41EquationsOfMotion = getEquationsOfMotion(dynamicsModel, propagator.equationType)
+    for tIndex::Int16 in Int16(2):Int16(length(tSpan))
+        if tIndex > Int16(2)
+            q0 = copy(getStateByIndex(arcOut, -1))
+            deleteStateAndTime!(arcOut, -1)
+        end
+        t0::Float64 = tSpan[tIndex-1]
+        tf::Float64 = tSpan[tIndex]
+        problem::DifferentialEquations.ODEProblem = DifferentialEquations.ODEProblem(computeDerivatives!, q0, (t0, tf), (EOMs,))
+        sol::DifferentialEquations.ODESolution = DifferentialEquations.solve(problem, propagator.integratorFactory.integrator, abstol = propagator.absTol, reltol = propagator.relTol, dtmax = propagator.maxStep, maxiters = propagator.maxEvaluationCount)
+        append!(arcOut.states, sol.u)
+        append!(arcOut.times, sol.t)
+    end
+
+    return arcOut
+end
+
+"""
     propagateWithEvent(propagator, callbackEvent, q0, tSpan, dynamicsModel)
 
 Return propagated arc
