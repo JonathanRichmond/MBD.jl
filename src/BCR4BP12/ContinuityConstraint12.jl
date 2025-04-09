c02f1a033b0e38a4fc4cb2711d24461dac853b32
Jonathan Richmond <richmo11@purdue.edu>
2025-04-09 17:37:42 -0400

    Updated getPartials_ConstraintWRTVariables function

--- src/BCR4BP12/ContinuityConstraint12.jl
+++ src/BCR4BP12/ContinuityConstraint12.jl
@@ -51,8 +51,8 @@ Return partial derivatives of constraint with respect to free variables
 """
 function getPartials_ConstraintWRTVariables(continuityConstraint::BCR4BP12ContinuityConstraint, freeVariableIndexMap::Dict{MBD.Variable, Int16}, freeVariableVector::Vector{Float64})
     numConstraints::Int16 = Int16(getNumConstraintRows(continuityConstraint))
-    STM::StaticArrays.SMatrix{6, 6, Float64} = StaticArrays.SMatrix{6, 6, Float64}(getPartials_FinalStateWRTInitialState!(continuityConstraint.segment))
-    propStateRate::StaticArrays.SVector{6, Float64} = StaticArrays.SVector{6, Float64}(getFinalStateRate!(continuityConstraint.segment))
+    STM::StaticArrays.SMatrix{7, 7, Float64} = StaticArrays.SMatrix{7, 7, Float64}(getPartials_FinalStateWRTInitialState!(continuityConstraint.segment))
+    propStateRate::StaticArrays.SVector{7, Float64} = StaticArrays.SVector{7, Float64}(getFinalStateRate!(continuityConstraint.segment))
     finalStateWRTTime::Matrix{Float64} = zeros(Float64, (numConstraints,1))
     finalStateWRTInitialState::Matrix{Float64} = zeros(Float64, (numConstraints,6))
     finalStateWRTTargetState::Matrix{Float64} = copy(finalStateWRTInitialState)
@@ -66,7 +66,7 @@ function getPartials_ConstraintWRTVariables(continuityConstraint::BCR4BP12Contin
     partials[continuityConstraint.segment.originNode.state] = finalStateWRTInitialState
     partials[continuityConstraint.segment.terminalNode.state] = finalStateWRTTargetState
     if !isEpochIndependent(continuityConstraint.segment.originNode.dynamicsModel)
-        dqdT::StaticArrays.SMatrix{6, 1, Float64} = StaticArrays.SMatrix{6, 1, Float64}(getPartials_FinalStateWRTEpoch!(continuityConstraint.segment))
+        dqdT::StaticArrays.SMatrix{7, 1, Float64} = StaticArrays.SMatrix{7, 1, Float64}(getPartials_FinalStateWRTEpoch!(continuityConstraint.segment))
         finalStateWRTEpoch::Matrix{float64} = zeros(Float64, (numConstraints,1))
         [finalStateWRTEpoch[index,1] = dqdT[continuityConstraint.constrainedIndices[index],1] for index in Int16(1):numConstraints]
         partials[continuityConstraint.segment.originNode.epoch] = finalStateWRTEpoch
