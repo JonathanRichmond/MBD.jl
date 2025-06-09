26de95e7778e27496128026952bece0135b04b3a
Jonathan Richmond <richmo11@purdue.edu>
2025-06-09 00:46:44 -0400

    Improved ConstraintVectorL2NormConvergenceCheck functions

--- src/corrections/ConstraintVectorL2NormConvergenceCheck.jl
+++ src/corrections/ConstraintVectorL2NormConvergenceCheck.jl
@@ -65,9 +65,16 @@ function isConverged(convergenceCheck::ConstraintVectorL2NormConvergenceCheck, p
         Logging.@info "Convergence check passed: norm = $constraintNorm ≤ $maxNorm"
 
         return true
-    else
-        Logging.@debug "Convergence check failed: norm = $constraintNorm > $maxNorm"
-
-        return false
     end
+    maxIndex::Int16 = Int16(argmax(constraintVec))
+    maxValue::Float64 = constraintVec[maxIndex]
+    for c in getConstraints(problem)
+        if get(problem.constraintIndexMap, c, -1) == maxIndex
+            Logging.@debug "Convergence check failed: norm = $constraintNorm > $maxNorm (maximum value: $maxValue from $(typeof(c)))"
+            break
+        end
+    end
+
+    Logging.@debug "Convergence check failed: norm = $constraintNorm > $maxNorm (unidentified maximum value: $maxValue)"
+    return false
 end
