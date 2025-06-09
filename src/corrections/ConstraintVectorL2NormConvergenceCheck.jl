bdeec2f0ea94cf08fb486def68b5dc0b88c96fe9
Jonathan Richmond <richmo11@purdue.edu>
2025-06-09 00:14:40 -0400

    Improved ConstraintVectorL2NormConvergenceCheck functions

--- src/corrections/ConstraintVectorL2NormConvergenceCheck.jl
+++ src/corrections/ConstraintVectorL2NormConvergenceCheck.jl
@@ -3,36 +3,64 @@ Constraint vector L2 norm convergence check wrapper
 
 Author: Jonathan Richmond
 C: 9/9/22
-U: 4/9/25
+U: 6/9/25
 """
 
-import LinearAlgebra
+import LinearAlgebra, Logging
 import MBD: ConstraintVectorL2NormConvergenceCheck
 
 export isConverged
 
 """
-    isConverged(constraintVectorL2NormConvergenceCheck, multipleShooterProblem)
+    isConverged(convergenceCheck, problem)
 
 Return true if problem is converged
 
 # Arguments
-- `constraintVectorL2NormConvergenceCheck::ConstraintVectorL2NormConvergenceCheck`: Constraint vector L2 norm convergence check object
-- `multipleShooterProblem::BCR4BP12MultipleShooterProblem`: BCR4BP P1-P2 multiple shooter problem object
+- `convergenceCheck::ConstraintVectorL2NormConvergenceCheck`: Constraint vector L2 norm convergence check object
+- `problem::BCR4BP12MultipleShooterProblem`: BCR4BP P1-P2 multiple shooter problem object
 """
-function isConverged(constraintVectorL2NormConvergenceCheck::ConstraintVectorL2NormConvergenceCheck, multipleShooterProblem::MBD.BCR4BP12MultipleShooterProblem)
-    return LinearAlgebra.norm(getConstraintVector!(multipleShooterProblem)) <= constraintVectorL2NormConvergenceCheck.maxVectorNorm
+function isConverged(convergenceCheck::ConstraintVectorL2NormConvergenceCheck, problem::MBD.BCR4BP12MultipleShooterProblem)
+    constraintVec::Vector{Float64} = getConstraintVector!(problem)
+    constraintNorm::Float64 = LinearAlgebra.norm(constraintVec)
+    maxNorm::Float64 = convergenceCheck.maxVectorNorm
+
+    Logging.@debug "Constraint vector L2 norm: $constraintNorm (threshold: $maxNorm)"
+
+    if constraintNorm <= maxNorm
+        Logging.@info "Convergence check passed: norm = $constraintNorm ≤ $maxNorm"
+
+        return true
+    else
+        Logging.@debug "Convergence check failed: norm = $constraintNorm > $maxNorm"
+
+        return false
+    end
 end
 
 """
-    isConverged(constraintVectorL2NormConvergenceCheck, multipleShooterProblem)
+    isConverged(convergenceCheck, problem)
 
 Return true if problem is converged
 
 # Arguments
-- `constraintVectorL2NormConvergenceCheck::ConstraintVectorL2NormConvergenceCheck`: Constraint vector L2 norm convergence check object
-- `multipleShooterProblem::CR3BPMultipleShooterProblem`: CR3BP multiple shooter problem object
+- `convergenceCheck::ConstraintVectorL2NormConvergenceCheck`: Constraint vector L2 norm convergence check object
+- `problem::CR3BPMultipleShooterProblem`: CR3BP multiple shooter problem object
 """
-function isConverged(constraintVectorL2NormConvergenceCheck::ConstraintVectorL2NormConvergenceCheck, multipleShooterProblem::MBD.CR3BPMultipleShooterProblem)
-    return LinearAlgebra.norm(getConstraintVector!(multipleShooterProblem)) <= constraintVectorL2NormConvergenceCheck.maxVectorNorm
+function isConverged(convergenceCheck::ConstraintVectorL2NormConvergenceCheck, problem::MBD.CR3BPMultipleShooterProblem)
+    constraintVec::Vector{Float64} = getConstraintVector!(problem)
+    constraintNorm::Float64 = LinearAlgebra.norm(constraintVec)
+    maxNorm::Float64 = convergenceCheck.maxVectorNorm
+
+    Logging.@debug "Constraint vector L2 norm: $constraintNorm (threshold: $maxNorm)"
+
+    if constraintNorm <= maxNorm
+        Logging.@info "Convergence check passed: norm = $constraintNorm ≤ $maxNorm"
+
+        return true
+    else
+        Logging.@debug "Convergence check failed: norm = $constraintNorm > $maxNorm"
+
+        return false
+    end
 end
