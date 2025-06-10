bde1babd0d29ff5dd686b8fb20ef372856136583
Jonathan Richmond <richmo11@purdue.edu>
2025-06-10 13:08:07 -0400

    Updated getManifoldArcByTime function

--- src/BCR4BP12/PeriodicOrbit.jl
+++ src/BCR4BP12/PeriodicOrbit.jl
@@ -57,8 +57,6 @@ function getManifoldArcByTime(periodicOrbit::BCR4BP12PeriodicOrbit, stability::S
     eigenvector::StaticArrays.SVector{7, Complex{Float64}} = StaticArrays.SVector{7, Complex{Float64}}(eigenvectors[:,index])
     propagator = MBD.Propagator()
     propagator.equationType = MBD.STM
-    theta4dot::Float64 = evaluateEquations(periodicOrbit.dynamicsModel, MBD.SIMPLE, 0.0, [0.9, 0, 0, 0, -0.3, 0, 0])[7]
-    initialCondition::Vector{Float64} = push!(periodicOrbit.initialCondition[1:6], periodicOrbit.initialCondition[7]+theta4dot*orbitTime*periodicOrbit.period)
     orbitArc::MBD.BCR4BP12Arc = propagate(propagator, appendExtraInitialConditions(periodicOrbit.dynamicsModel, initialCondition, MBD.STM), [0, orbitTime*periodicOrbit.period], periodicOrbit.dynamicsModel)
     stateSize::Int64 = getStateSize(periodicOrbit.dynamicsModel, MBD.STM)
     q::StaticArrays.SVector{stateSize, Float64} = StaticArrays.SVector{stateSize, Float64}(getStateByIndex(orbitArc, -1))
