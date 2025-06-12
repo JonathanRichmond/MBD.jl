4d567da77004e5d409b32622dd9996002bf8bb03
Jonathan Richmond <richmo11@purdue.edu>
2025-06-12 12:41:34 -0400

    Debugging

--- src/BCR4BP12/MultipleShooter12.jl
+++ src/BCR4BP12/MultipleShooter12.jl
@@ -27,7 +27,6 @@ function solve!(multipleShooter::BCR4BP12MultipleShooter, initialGuess::MBD.BCR4
     while !isConverged(multipleShooter.convergenceCheck, solutionInProgress) && (multipleShooter.recentIterationCount < multipleShooter.maxIterations)
         if multipleShooter.recentIterationCount > 0
             freeVariableStep::StaticArrays.SVector{Int64(numFreeVariables), Float64} = solveUpdateEquation(multipleShooter, solutionInProgress)
-            println(freeVariableStep)
             freeVariableVector::Vector{Float64} = getFreeVariableVector!(solutionInProgress)+freeVariableStep
             setFreeVariableVector!(solutionInProgress, freeVariableVector)
         end
