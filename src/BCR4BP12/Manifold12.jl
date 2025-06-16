3ee977b0b1482d0afed455b57e2a4f870dd4f280
Jonathan Richmond <richmo11@purdue.edu>
2025-06-16 14:29:21 -0500

    Created Manifold12.jl wrapper

--- src/BCR4BP12/Manifold12.jl
+++ src/BCR4BP12/Manifold12.jl
@@ -23,7 +23,7 @@ function stopCrashes(manifold::BCR4BP12Manifold)
     crashEvent = DifferentialEquations.VectorContinuousCallback(primaryDistanceCondition, terminateAffect!, 3)
     manifoldArcs::Vector{MBD.BCR4BP12ManifoldArc} = Vector{MBD.BCR4BP12ManifoldArc}(undef, length(manifold.initialConditions))
     for a::Int64 = 1:length(manifold.initialConditions)
-        arc::MBD.BCR4BP12Arc = propagateWithEvent(propagator, crashEvent, real(manifold.initialConditions[a]), [0.0, manifold.TOF], manifold.periodicOrbit.dynamicsModel, [manifold.periodicOrbit.dynamicsModel, manifold.periodicOrbit.dynamicsModel.systemData.primaryData[1].bodyRadius, manifold.periodicOrbit.dynamicsModel.systemData.primaryData[2].bodyRadius, manifold.periodicOrbit.dynamicsModel.systemData.primaryData[3].bodyRadius])
+        arc::MBD.BCR4BP12Arc = propagateWithEvent(propagator, crashEvent, real(manifold.initialConditions[a]), [0.0, manifold.TOF], manifold.periodicOrbit.dynamicsModel, [manifold.periodicOrbit.dynamicsModel, manifold.periodicOrbit.dynamicsModel.systemData.primaryData[1].bodyRadius/getCharLength(manifold.periodicOrbit.dynamicsModel), manifold.periodicOrbit.dynamicsModel.systemData.primaryData[2].bodyRadius/getCharLength(manifold.periodicOrbit.dynamicsModel), manifold.periodicOrbit.dynamicsModel.systemData.primaryData[3].bodyRadius/getCharLength(manifold.periodicOrbit.dynamicsModel)])
         manifoldArcs[a] = MBD.BCR4BP12ManifoldArc(manifold.periodicOrbit, manifold.orbitTimes[a], manifold.ds[a], manifold.initialConditions[a], getTimeByIndex(arc, -1))
     end
 
