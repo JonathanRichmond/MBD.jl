237a7d71ecdb79a12de96e2fffba5e7c3e2c5539
Jonathan Richmond <richmo11@purdue.edu>
2025-06-16 14:47:41 -0500

    Created Manifold12.jl wrapper

--- src/propagation/EventFunctions.jl
+++ src/propagation/EventFunctions.jl
@@ -95,7 +95,6 @@ function primaryDistanceCondition(output, state::Vector{Float64}, time::Float64,
     d2::Float64 = LinearAlgebra.norm(state[1:3]-r2)
     d4::Float64 = LinearAlgebra.norm(state[1:3]-r4)
     output = [d1-integrator.p[3], d2-integrator.p[4], d4-integrator.p[5]]
-    println(output)
 end
 
 """
