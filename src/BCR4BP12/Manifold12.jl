e265819435d3408e29561823480e388cae5a3fa3
Jonathan Richmond <richmo11@purdue.edu>
2025-06-16 13:58:54 -0500

    Created Manifold12.jl wrapper

--- src/BCR4BP12/Manifold12.jl
+++ src/BCR4BP12/Manifold12.jl
@@ -23,7 +23,7 @@ function stopCrashes(manifold::BCR4BP12Manifold)
     crashEvent = DifferentialEquations.VectorContinuousCallback(primaryDistanceCondition, terminateAffect!, 3)
     manifoldArcs::Vector{MBD.BCR4BP12ManifoldArc} = Vector{MBD.BCR4BP12ManifoldArc}(undef, length(manifold.initialConditions))
     for a::Int64 = 1:length(manifold.initialConditions)
-        arc::MBD.BCR4BP12Arc = propagateWithEvent(propagator, crashEvent, manifold.initialConditions[a], [0.0, manifold.TOF], manifold.periodicOrbit.dynamicsModel)
+        arc::MBD.BCR4BP12Arc = propagateWithEvent(propagator, crashEvent, real(manifold.initialConditions[a]), [0.0, manifold.TOF], manifold.periodicOrbit.dynamicsModel)
         manifoldArcs[a] = MBD.BCR4BP12ManifoldArc(manifold.periodicOrbit, manifold.orbitTimes[a], manifold.ds[a], manifold.initialConditions[a], TOF = getTimeByIndex(arc, -1))
     end
 
