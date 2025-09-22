f73ef6270c95f2b56891012ec5d5cf13a7a6728e
Jonathan Richmond <richmo11@purdue.edu>
2025-09-22 11:28:54 -0400

    Updated computeDerivatives! function

--- src/Keplerian/EquationsOfMotion.jl
+++ src/Keplerian/EquationsOfMotion.jl
@@ -15,17 +15,18 @@ export computeDerivatives!, getMassParameter, getStateSize
 Return time derivative of state vector
 
 # Arguments
-- `qdot::Vector{Float64}`: Time derivative of state vector [ndim]
-- `q::Vector{Float64}`: State vector [ndim]
+- `qdot::Vector{Float64}`: Time derivative of state vector [dim]
+- `q::Vector{Float64}`: State vector [dim]
 - `params::Tuple{KEquationsOfMotion}`: Propagation parameters
-- `t::Float64`: Time [ndim]
+- `t::Float64`: Time [dim]
 """
 function computeDerivatives!(qdot::Vector{Float64}, q::Vector{Float64}, params::Tuple{KEquationsOfMotion}, t::Float64)
+    mu::Float64 = getMassParameter(params[1])
     r::Float64 = sqrt(q[1]^2+q[2]^2+q[3]^2)
     qdot[1:3] = q[4:6]
-    qdot[4:6] = -1*q[1:3]./r^3
+    qdot[4:6] = -mu*q[1:3]./r^3
     if params[1].equationType != MBD.SIMPLE
-        A_21::Matrix{Float64} = (3/r^5).*q[1:3]*q[1:3]'-(1/r^3)*LinearAlgebra.I
+        A_21::Matrix{Float64} = (3*mu/r^5).*q[1:3]*q[1:3]'-(mu/r^3)*LinearAlgebra.I
         qdot[7:42] = reshape([zeros(Float64, (3,3)) LinearAlgebra.I; A_21 zeros(Float64, (3,3))]', (1,36))
     end
     if params[1].equationType == MBD.ARCLENGTH
