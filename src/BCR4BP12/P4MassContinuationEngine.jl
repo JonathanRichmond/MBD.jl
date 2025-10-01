e10a9876e42abfc1cc39bbbd8bb7a9d26f911473
Jonathan Richmond <richmo11@purdue.edu>
2025-10-01 14:08:27 -0400

    Fixing jump check

--- src/BCR4BP12/P4MassContinuationEngine.jl
+++ src/BCR4BP12/P4MassContinuationEngine.jl
@@ -194,6 +194,7 @@ function tryConverging!(p4MassContinuationEngine::P4MassContinuationEngine)
                 if typeof(jumpCheck) == MBD.BoundingBoxJumpCheck
                     for (index::MBD.Variable, value::Int16) in p4MassContinuationEngine.dataInProgress.previousSolution.freeVariableIndexMap
                         if index.name == jumpCheck.paramName
+                            println("Name matches")
                             addBounds!(jumpCheck, p4MassContinuationEngine.dataInProgress.previousSolution, index, jumpCheck.paramBounds)
                             p4MassContinuationEngine.dataInProgress.converging = isFamilyMember(jumpCheck, p4MassContinuationEngine.dataInProgress)
                             (!p4MassContinuationEngine.dataInProgress.converging && p4MassContinuationEngine.printProgress) && println("Solution jumped")
