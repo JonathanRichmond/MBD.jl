82788b28384d5da23c8da08bb7952aaa80388211
Jonathan Richmond <richmo11@purdue.edu>
2025-10-27 14:35:16 -0400

    Added propagateWithEvents function

--- src/propagation/Propagator.jl
+++ src/propagation/Propagator.jl
@@ -314,7 +314,7 @@ function propagateWithEvents(propagator::Propagator, callbackEvent::Differential
         end
         t0::Float64 = tSpan[tIndex-1]
         tf::Float64 = tSpan[tIndex]
-        problem = DifferentialEquations.ODEProblem(computeDerivatives!, q0, (t0, tf), (EOMs, :none, params...))
+        problem = DifferentialEquations.ODEProblem(computeDerivatives!, q0, (t0, tf), (EOMs, params...))
         sol::DifferentialEquations.ODESolution = DifferentialEquations.solve(problem, propagator.integratorFactory.integrator, callback = callbackEvent, abstol = propagator.absTol, reltol = propagator.relTol, dtmax = propagator.maxStep, maxiters = propagator.maxEvaluationCount)
         append!(arcOut.states, sol.u)
         append!(arcOut.times, sol.t)
