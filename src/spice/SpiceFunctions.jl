255585e6d316f8ba0f8a17fc5eb6b63f58eb089b
Jonathan Richmond <richmo11@purdue.edu>
2026-02-03 01:35:22 -0500

    Testing Ephemerides.jl

--- src/spice/SpiceFunctions.jl
+++ src/spice/SpiceFunctions.jl
@@ -38,20 +38,35 @@ function getEphemerides(initialEpoch::String, times::Vector{Float64}, targetBody
 end
 
 """
-    getEphemerides(eph, initialEpochTime, times, targetBodyID, observerBodyID)
+    getEphemerides(eph, initialEpochTime, times, targetBodyID, observerBodyID, referenceBodyID; frame)
 
-Return ephemerides in ECLIPJ2000 frame?
+Return ephemerides
 
 # Arguments
 - `eph::EphemerisProvider`: Ephemeris provider object
 - `initialEpochTime::Float64`: Initial epoch time [s]
 - `times::Vector{Float64}`: Times since initial epoch
 - `targetBodyID::Int64`: Target body SPICE ID for ephemerides
-- `observerBodyID::Int64`: Body SPICE ID for ephemerides reference
+- `observerBodyID::Int64`: Observer body SPICE ID for ephemerides
+- `referenceBodyID::Int64`: Body SPICE ID for ephemerides reference (common to both)
+- `frame::String`: Reference frame (default = "ECLIPJ2000")
 """
-function getEphemerides(eph::Ephemerides.EphemerisProvider, initialEpochTime::Float64, times::Vector{Float64}, targetBodyID::Int64, observerBodyID::Int64)
+function getEphemerides(eph::Ephemerides.EphemerisProvider, initialEpochTime::Float64, times::Vector{Float64}, targetBodyID::Int64, observerBodyID::Int64, referenceBodyID::Int64; frame::String = "ECLIPJ2000")
     ephemerisTimes::Vector{Float64} = initialEpochTime .+ times
-    ephemerisStates::Vector{Vector{Float64}} = [Ephemerides.ephem_vector6(eph, observerBodyID, targetBodyID, et) for et in ephemerisTimes]
-    
+    if frame == "ECLIPJ2000"
+        i::Float64 = 23.43929111*pi/180
+        R::Matrix{Float64} = [1 0 0; 0 cos(i) sin(i); 0 -sin(i) cos(i)]
+        N::Matrix{Float64} = [R zeros(Float64, (3,3)); zeros(Float64, (3,3)) R]
+        ephemerisStates::Vector{Vector{Float64}} = Vector{Vector{Float64}}(undef, length(ephemerisTimes))
+        for et::Int64 in 1:length(ephemerisTimes)
+            eqState::Vector{Float64} = Ephemerides.ephem_vector6(eph, referenceBodyID, targetBodyID, ephemerisTimes[et])-Ephemerides.ephem_vector6(eph, referenceBodyID, observerBodyID, ephemerisTimes[et])
+            ephemerisStates[et] = N*eqState
+        end
+    elseif frame == "J2000"
+        ephemerisStates::Vector{Vector{Float64}} = [Ephemerides.ephem_vector6(eph, referenceBodyID, targetBodyID, et)-Ephemerides.ephem_vector6(eph, referenceBodyID, observerBodyID, et) for et in ephemerisTimes]
+    else
+        throw(ArgumentError("Frame not supported"))
+    end
+
     return (ephemerisStates, ephemerisTimes)
 end
