0c7b997e5bf3add959e4f43f0d34e93072ab8c38
Jonathan Richmond <richmo11@purdue.edu>
2026-02-03 01:37:37 -0500

    Testing Ephemerides.jl

--- src/CR3BP/DynamicsModel.jl
+++ src/CR3BP/DynamicsModel.jl
@@ -571,7 +571,7 @@ function rotatingToPrimaryEclipJ2000(dynamicsModel::CR3BPDynamicsModel, eph::Eph
     (Int16(length(states)) == numTimes) || throw(ArgumentError("Number of state vectors, $(length(states)), must match number of times, $(length(times))"))
     lstar::Float64 = getCharLength(dynamicsModel)
     tstar::Float64 = getCharTime(dynamicsModel)
-    bodyInitialStateDim::Vector{Float64} = getEphemerides(eph, initialEpochTime, [0.0], dynamicsModel.systemData.primaryData[2].spiceID, dynamicsModel.systemData.primaryData[1].spiceID)[1][1]
+    bodyInitialStateDim::Vector{Float64} = getEphemerides(eph, initialEpochTime, [0.0], dynamicsModel.systemData.primaryData[2].spiceID, dynamicsModel.systemData.primaryData[1].spiceID, firstdigit(dynamicsModel.systemData.primaryData[1].spiceID))[1][1]
     primary::MBD.BodyData = dynamicsModel.systemData.primaryData[1]
     bodySPICEElements::StaticArrays.MVector{20, Float64} = StaticArrays.MVector{20, Float64}(SPICE.oscltx(bodyInitialStateDim, initialEpochTime, primary.gravParam))
     (dynamicsModel.systemData.primaryNames[2] == "Earth") && (bodySPICEElements[3] = 0.0)
